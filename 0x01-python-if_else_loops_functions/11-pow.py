1027ebef14d38802815c17b356913aa63e1e509e
jimmyMithamo <j1mithamo@gmail.com>
2023-11-28 22:48:32 +0300

    python files

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author -james mithamo
+
+def pow(a, b):
+    return (a ** b)
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author - james mithamo
+
+def print_last_digit(number):
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
