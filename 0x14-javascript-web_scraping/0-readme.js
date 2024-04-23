2c1231ca935ef5133cbbc105dbd4530ba336d650
jimmyMithamo <j1mithamo@gmail.com>
2024-04-23 19:54:30 +0300

    semistandard error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-//a script that reads content from a file
+// a script that reads content from a file
 
-const fs = require("fs")
+const fs = require('fs');
 const path = process.argv[2];
 
-fs.readFile(path, "utf-8", (error, data) => {
-    if(error){
-        console.log(error);
-    }else{
-        console.log(data);
-    }
+fs.readFile(path, 'utf-8', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
 });
