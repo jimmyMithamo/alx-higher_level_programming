81ba95a45eddd62cbe33cd49537b937a9453e113
jimmyMithamo <j1mithamo@gmail.com>
2024-04-23 19:50:05 +0300

    error

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-// a script that writes into  a file
+// A script that writes a string to a file
 
 const fs = require('fs');
-const path = process.argv[2];
-const messageString = process.argv[3];
+const file = process.argv[2];
+const write_ = process.argv[3];
 
-fs.writeFile(path, messageString, 'utf-8', (error) => {
+fs.writeFile(file, write_, 'utf8', function (error) {
   if (error) {
     console.log(error);
   }
